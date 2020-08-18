f1c31606eb3fed6d3983e1fba9e1f83473f84275
Andreia de Moraes <andreiadmrs@gmail.com>
2020-08-18 00:07:37 -0300

    exercicio do curso da udemy csharp

---
+++ ExerciciosSobreEstruturaSequencial5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciciosSobreEstruturaSequencial5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o
+código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago.*/
+
+            Console.WriteLine("Código da primeira peça:");
+            String primeiraPeca = Console.ReadLine();
+            Console.WriteLine("Quantidade da primeira peça:");
+            int qntPrimeiraPeca = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor da primeira peça:");
+            double valorPrimeiraPeca = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Código da segunda peça:");
+            String segundaPeca = Console.ReadLine();
+            Console.WriteLine("Quantidade da segunda peça:");
+           int qntSegundaPeca = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor da segunda peça:");
+            double valorSegundaPeca = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valorTotal = qntSegundaPeca * valorSegundaPeca + qntPrimeiraPeca * valorPrimeiraPeca;
+
+            Console.WriteLine("O valor total a ser pago é R$:" +valorTotal.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
